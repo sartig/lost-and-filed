3aa707a32dae093a794d5331aa83d5b07d49d0ce
sartig <sartigolle@gmail.com>
2021-01-30 21:45:51 +0800

    Added randomize on spawn function

--- Lost & Filed/Assets/Scripts/RandomizeColours.cs
+++ Lost & Filed/Assets/Scripts/RandomizeColours.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class RandomizeColours : MonoBehaviour
 {
+    public bool randomizeOnSpawn = false;
+
+    void Start() {
+        if(randomizeOnSpawn) {
+            Randomize();
+        }
+    }
+
     public void Randomize() {
         for(int i = 0; i < transform.childCount; i++) {
             if(transform.GetChild(i).childCount>0) {
